e37ad6182681536b587ecbd9e912e2b81b083ab7
Hizbullah Abdul Aziz <archbung@gmail.com>
2020-02-09 07:10:23 +0100

    Rename dracut script

---
+++ dot_local/bin/executable_dracut-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+args=('-H' '--no-hostonly-cmdline')
+
+while read -r line; do
+  if [[ $line = usr/lib/modules/+([^/])/pkgbase ]]; then
+    mapfile -O ${#pkgbase[@]} -t pkgbase < "/$line"
+    kver=${line#"usr/lib/modules/"}
+    kver=${kver%"/pkgbase"}
+
+    install -Dm0644 "/${line%'/pkgbase'}/vmlinuz" "/boot/vmlinuz-linux-${pkgbase}"
+    dracut "${args[@]}" -f /boot/initramfs-"${pkgbase[@]}".img --kver "${kver[@]}"
+    dracut -f /boot/initramfs-"${pkgbase[@]}"-fallback.img --kver "${kver[@]}"
+  fi
+done
